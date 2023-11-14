7c482de13e45f771a06b1e973f6324655cc7137d
AryanShuklaa <92904286+AryanShuklaa@users.noreply.github.com>
2023-11-14 12:03:55 +0530

    Create authenticate.php

---
+++ authenticate.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetching user data
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$user' AND password='$pass'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $_SESSION['user'] = $user;
+    header('Location: upload.html');
+} else {
+    echo "Invalid credentials";
+}
+$conn->close();
+?>
